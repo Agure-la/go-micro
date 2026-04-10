04b1d669dbd0710d723d6ed4c8782898700062cf
Agure-la <lameckagure@gmail.com>
2026-04-10 09:57:39 +0300

    main: added db setup on main

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
- package main
+package main
 
-func main(){
+import (
+	"go-microservice/internal/database"
+	"go-microservice/internal/server"
+	"log"
+)
 
+func main() {
+	db, err := database.NewDatabaseClient()
+	if err != nil {
+		log.Fatalf("failed to initialize Database Client: %s", err)
+	}
+	srv := server.NewEchoServer(db)
+	if err := srv.Start();err != nil {
+		log.Fatal(err.Error())
+	}
 }
