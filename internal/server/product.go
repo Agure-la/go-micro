70d43c93edd9ddf225349cc92a987d8360fb8356
Agure-la <lameckagure@gmail.com>
2026-04-13 15:15:52 +0300

    Vendor model: added vendor's model

--- internal/server/product.go
+++ internal/server/product.go
@@ -2,4 +2,7 @@ package server
 
 type Product struct{
 	ProductID string `gorm:"primaryKey" json:"productId"`
+	Name      string  `json:"name"`
+	Price     float32  `gorm:"type:numeric" json:"price"`
+	VendorID  string   `json:"vendorId"`
 }
